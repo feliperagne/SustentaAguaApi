e74065e76e734944ced18c8ea2291d139f0a225f
Felipe Ragne <frsilveira01@outlook.com>
2023-09-11 19:30:53 -0300

    funcionando

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -11,21 +12,13 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request)
     {
-        dd($request->all());
         $user = User::create($request->all());
-        if ($request->hasFile('profile_image') && $request->file('profile_image')->isValid()) {
-            $profileImageName = uniqid('profile_') . '.' . $request->file('profile_image')->extension();
-            $request->file('profile_image')->storeAs('profile_images', $profileImageName, 'public');
-    
-            // Salva o caminho da imagem no banco de dados
-            $user->profile_image = 'profile_images/' . $profileImageName;
-            $user->save();
-        }
-    
+        
+        $user->save();
+        
         return [
-            'status' => 1,
-            'data' => $user,
-            'profile_image' => $user->profile_image ?? null,
+            'status' => '1',
+            'data' => $user
         ];
     }
     
--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\LowercaseRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreUserRequest extends FormRequest
@@ -22,25 +23,26 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-                "username" => "required",
-                "email" => "required|unique:users,email|email",
+                "username" => ["required",'regex:/^\S*$/u'],
+                "email" => "required|unique:users,email|email:rfc,dns",
                 "password" => "required|min:3",
-                'profile_image' => 'required|image|mimes: jpg,jpeg,png|max:6144',
+               //'profile_image' => 'required|image|mimes: jpg,jpeg,png|max:6144',
         ];
     }
 
     public function messages():array{
          return [
             "username.required" => "O campo de nome de usuário é obrigatório.",
+            'username.regex' => 'o campo de usuário não deve conter espaços!',
             "email.required" => "O campo de email é obrigatório.",
             "email.unique" => "Esse email já está sendo usado por outra pessoa.",
             "email.email" => "Por favor, informe um endereço de email válido.",
             'password.required' => "O campo de senha é obrigatório.",
             "password.min" => "Por favor, informa uma senha com mais de 3 caracteres!",
-            'profile_image.required' => 'Adicione uma foto de perfil para você!',
+            /*'profile_image.required' => 'Adicione uma foto de perfil para você!',
             'profile_image.image' => 'A imagem que você selecionou não é uma imagem válida!',
             'profile_image.mimes' => 'A imagem deve ter apenas os formatos JPG, JPEG e PNG!',
-            'profile_image.max' => 'A imagem não deve exceder 6MB!', // Defina o tamanho máximo em bytes
+            'profile_image.max' => 'A imagem não deve exceder 6MB!', // Defina o tamanho máximo em bytes*/
         ];
     }
 }
